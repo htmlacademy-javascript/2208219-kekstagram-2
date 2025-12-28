76ddcb5df067c7b0714e974471ab1efc3eddc3eb
Keks <keks@htmlacademy.ru>
2025-12-29 00:36:55 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/picturecreator.js
@@ -0,0 +1,20 @@
+import { createPhotos } from './createphotos.js';
+import * as consts from './constants.js';
+
+const pictures = document.querySelector('.pictures');
+const picture = document.querySelector('#picture').content.querySelector('.picture');
+const fragment = document.createDocumentFragment();
+const photos = createPhotos(consts.OBJCOUNT, consts.MAXLIKE, consts.MINLIKE, consts.COMMENTCOUNT, consts.AVATARCOUNT);
+
+function renderPictures() {
+  photos.forEach(({url, likes, description, comments}) => {
+    const pictureElement = picture.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    fragment.appendChild(pictureElement);
+  });
+}
+renderPictures();
+pictures.appendChild(fragment);
